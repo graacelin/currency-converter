9cb7c4b16ea5d5d22f83dd82b408dc5dd79cb1af
Grace Lin <grace02lin@gmail.com>
2021-11-14 13:41:33 -0800

    added api call for currency rates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import chimp from './img/monke.jpg'
 import './App.css';
 import Row from './Row'
 
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
 function App() {
+    const [currencyOptions, setCurrencyOptions] = useState([])
+    console.log(currencyOptions)
+
+    useEffect(() => {
+        fetch(BASE_URL)
+        .then(res => res.json())
+        .then(data => {
+            setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        })
+    }, [])
+
     return (
         <>
             <img src={chimp} class="chimp" />
