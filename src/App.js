04b127e1372702d56dc6a752ff9c776d78926bc0
JoshFung <joshuagyfung@gmail.com>
2021-11-14 13:07:19 -0800

    base React site built

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import chimp from './img/monke.jpg'
+import './App.css';
+
+function App() {
+    return (
+        // <h1> Testing </h1>
+        <img src={chimp} class="chimp" />
+        // <body>
+        //     <img src='img/monke.jpg' class="monke-img" alt=''/>
+        // </body>
+    );
+}
+
+export default App;
