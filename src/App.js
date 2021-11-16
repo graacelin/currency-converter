54297dcbe7ab0b9cf94e9812ba3b66945613ecd9
Grace Lin <grace02lin@gmail.com>
2021-11-15 22:06:29 -0800

    simplified code & added conversion message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import DarkModeToggle from "react-dark-mode-toggle";
 
 const BASE_URL = "https://v6.exchangerate-api.com/v6/4efd06e36cfb923dccc7819c/latest/CAD"
 
+const ModeSelector = () => {
+    const [isDarkMode, setIsDarkMode] = useState(() => false);
+
+    function changeTheme() {
+        setIsDarkMode(!isDarkMode)
+        if (isDarkMode) {
+            document.documentElement.setAttribute("data-theme", "light") // set theme light
+            localStorage.setItem("data-theme", 'light') // save theme to local storage
+        } else {
+            document.documentElement.setAttribute("data-theme", "dark") // set theme to dark
+            localStorage.setItem("data-theme", "dark") // save theme to local storage
+        }
+    }
+
+    return (<DarkModeToggle onChange={changeTheme} checked={isDarkMode} size={60} className="darkModeToggle" />)
+}
+
 function App() {
     const [currencyOptions, setCurrencyOptions] = useState([])
     const [fromCurrency, setFromCurrency] = useState('CAD')
@@ -13,10 +30,6 @@ function App() {
     const [exchangeRate, setExchangeRate] = useState(1)
     const [amount, setAmount] = useState(1)
     const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
-    const [countryFlagFrom, setCountryFlagFrom] = useState()
-    const [countryFlagTo, setCountryFlagTo] = useState()
-    const [isDarkMode, setIsDarkMode] = useState(() => false);
-
 
     let toAmount, fromAmount
     if (amountInFromCurrency) {
@@ -33,75 +46,43 @@ function App() {
             setFromCurrency(data.base_code);
             setToCurrency(Object.keys(data.conversion_rates)[0]);
             setExchangeRate(data.conversion_rates[Object.keys(data.conversion_rates)[0]]);
-            setCountryFlagTo(`flag-icon flag-icon-ca`)
-            setCountryFlagFrom(`flag-icon flag-icon-ca`)
         })
     }, [])
-    
 
     useEffect(() => {
         if (fromCurrency != null && toCurrency != null) {
-          fetch(`https://v6.exchangerate-api.com/v6/4efd06e36cfb923dccc7819c/pair/${fromCurrency}/${toCurrency}`)
-            .then(res => res.json())
-            .then(data => setExchangeRate(data.conversion_rate))
+            fetch(`https://v6.exchangerate-api.com/v6/4efd06e36cfb923dccc7819c/pair/${fromCurrency}/${toCurrency}`)
+                .then(res => res.json())
+                .then(data => setExchangeRate(data.conversion_rate))
         }
-      }, [fromCurrency, toCurrency])
+    }, [fromCurrency, toCurrency])
 
+    const countryFlagFrom = `flag-icon flag-icon-${String(country_list[fromCurrency]).toLowerCase()}`
+    const countryFlagTo = `flag-icon flag-icon-${String(country_list[toCurrency]).toLowerCase()}`
 
-    function handleFromAmountChange(e) {
+    const handleFromAmountChange = (e) => {
         setAmount(e.target.value)
         setAmountInFromCurrency(true)
-        
     }
 
-    function handleToAmountChange(e) {
-        setAmountInFromCurrency(false)
+    const handleToAmountChange = (e) => {
         setAmount(e.target.value)
-        console.log(toCurrency)
+        setAmountInFromCurrency(false)
     }
 
-
-    function handleFromCurrencyChange(e) {
-        setFromCurrency(e.target.value)
-        console.log(country_list)
-        for(let code in country_list) {
-            console.log(code)
-            if(code === e.target.value) {
-                let country = String(country_list[e.target.value]).toLowerCase();
-                console.log(country)
-                setCountryFlagFrom(`flag-icon flag-icon-${country}`)
-            }
-        }
-    }
-
-    function handleToCurrencyChange(e) {
+    const handleToCurrencyChange = (e) => {
         setToCurrency(e.target.value)
-        for(let code in country_list) {
-            if(code === e.target.value) {
-                let country = String(country_list[e.target.value]).toLowerCase();
-                console.log(country)
-                setCountryFlagTo(`flag-icon flag-icon-${country}`)
-            }
-        }
     }
 
-    function changeTheme() {
-        setIsDarkMode(!isDarkMode)
-        if (isDarkMode){
-            document.documentElement.setAttribute("data-theme", "light") // set theme light
-            localStorage.setItem("data-theme", 'light') // save theme to local storage
-        } else{
-            document.documentElement.setAttribute("data-theme", "dark") // set theme to dark
-            localStorage.setItem("data-theme", "dark") // save theme to local storage
-        }  
+    const handleFromCurrencyChange = (e) => {
+        setFromCurrency(e.target.value)
     }
 
     return (
-        <>      
-            <DarkModeToggle onChange={changeTheme} checked={isDarkMode} size={60} className="darkModeToggle" />
+        <>
             <h1> Currency Converter </h1>
-
-            <Row 
+            <ModeSelector />
+            <Row
                 currencyOptions={currencyOptions}
                 selectedCurrency={fromCurrency}
                 onChangeAmount={handleFromAmountChange}
@@ -110,16 +91,19 @@ function App() {
                 countryFlag={countryFlagFrom}
             />
             <div className="equals">=</div>
-                <Row 
-                    currencyOptions={currencyOptions}
-                    selectedCurrency={toCurrency}
-                    onChangeAmount={handleToAmountChange}
-                    amount={toAmount}
-                    onChangeCurrency={handleToCurrencyChange}
-                    countryFlag={countryFlagTo}
-                    
-                /> 
-            
+            <Row
+                currencyOptions={currencyOptions}
+                selectedCurrency={toCurrency}
+                onChangeAmount={handleToAmountChange}
+                amount={toAmount}
+                onChangeCurrency={handleToCurrencyChange}
+                countryFlag={countryFlagTo}
+            />
+
+            <div className="conversion-rate">
+                1 {fromCurrency} = {exchangeRate} {toCurrency}
+            </div>
+
         </>
 
     );
